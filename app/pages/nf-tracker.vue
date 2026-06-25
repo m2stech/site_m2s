841d3273ac5fa1b9e544b4617468890639327f62
Mauricio Santos <mauricio@m2stech.tur.br>
2026-06-25 18:51:14 -0300

    Adiciona imagem interface.png necessária para página NF Tracker

--- app/pages/nf-tracker.vue
+++ app/pages/nf-tracker.vue
@@ -106,7 +106,7 @@
         <div class="interface-mockup">
           <div class="notebook-frame">
             <img 
-              src="/images/interface.PNG" 
+              src="/images/interface.png" 
               alt="Interface do NF Tracker mostrando seleção de certificado, download automático e conversão para Excel"
               class="interface-image"
               loading="lazy"
